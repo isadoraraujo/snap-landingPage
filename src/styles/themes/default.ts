d2737a124743fdd7ab18a121768a8ab316f5f6b6
Isadora Áraújo <isadoraraujo13@gmail.com>
2022-10-15 18:24:45 -0300

    config

---
+++ src/styles/themes/default.ts
@@ -0,0 +1,7 @@
+export const defaultTheme = {
+    colors: {
+        'Almost-White': 'hsl(0, 0%, 98%)',
+        'Medium-Gray': 'hsl(0, 0%, 41%)',
+        'Almost-Black': 'hsl(0, 0%, 8%)'
+    } as const
+}
