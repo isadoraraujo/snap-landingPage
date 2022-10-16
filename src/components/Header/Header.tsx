424d22c30ab92e157edac6ac3c1c0db44a5b15b6
Flavio <fjcf@poli.br>
2022-10-15 22:28:13 -0300

    fixed all bugs

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -4,7 +4,6 @@ export const Header = () => {
     return (
         <HeaderContainer>
             <NavLinks>
-            <h1>I</h1>
             <figure>
                 <img src="logo.svg" alt="Logo" />
             </figure>
