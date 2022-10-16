586a9af721641dfad6fe415635d410f038531df7
Isadora Araújo <93020656+isadoraraujo@users.noreply.github.com>
2022-10-16 12:38:06 -0300

    Revert "Isadora"

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,55 +1,21 @@
-import { Close, Content, HeaderContainer, HeaderMobile, NavLinks, NavRegister, Overlay, Trigger } from "./styles";
-import * as Dialog from '@radix-ui/react-dialog';
+import { HeaderContainer, NavLinks, NavRegister } from "./styles";
 
 export const Header = () => {
-    const isWidthDesktop = window.screen.width;
-
     return (
         <HeaderContainer>
-            {isWidthDesktop >= 1024 ? (
-                <>
-                    <NavLinks>
-                        <figure>
-                            <img src="logo.svg" alt="Logo" />
-                        </figure>
-                        <a>Features</a>
-                        <a>Company</a>
-                        <a>Careers</a>
-                        <a>About</a>
-                    </NavLinks>
-                    <NavRegister>
-                        <a>Login</a>
-                        <button><a>Register</a></button>
-                    </NavRegister>
-                </>
-            ) : (
-                <HeaderMobile>
-                    <figure>
-                            <img src="logo.svg" alt="Logo" />
-                        </figure>
-                    <Dialog.Root>
-                        <Trigger asChild>
-                            <figure>
-                                <img src="icon-menu.svg" />
-                            </figure>
-                        </Trigger>
-                        <Dialog.Portal>
-                            <Overlay />
-                            <Content>
-                                <Close><img src="icon-close-menu.svg" /></Close>
-                                <a>Features</a>
-                                <a>Company</a>
-                                <a>Careers</a>
-                                <a>About</a>
-                                <NavRegister>
-                                    <a>Login</a>
-                                    <button><a>Register</a></button>
-                                </NavRegister>
-                            </Content>
-                        </Dialog.Portal>
-                    </Dialog.Root>
-                </HeaderMobile>
-            )}
+            <NavLinks>
+            <figure>
+                <img src="logo.svg" alt="Logo" />
+            </figure>
+                <a>Features</a>
+                <a>Company</a>
+                <a>Careers</a>
+                <a>About</a>
+            </NavLinks>
+            <NavRegister>
+                <a>Login</a>
+                <button><a>Register</a></button>
+            </NavRegister>
         </HeaderContainer>
     )
 }
--- src/components/Main/styles.ts
+++ src/components/Main/styles.ts
@@ -5,6 +5,7 @@ export const Container = styled.div`
     display: flex;
     flex-direction: column;
     align-items: center;
+    border: 1px solid blue;
   }
 
   @media (min-width: 1440px) {
