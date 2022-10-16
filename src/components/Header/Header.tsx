3cd92e8b7e7e856dcf2648881ad662c7759e3b6c
Flavio <fjcf@poli.br>
2022-10-16 12:44:44 -0300

    modal mobile version

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,43 +1,76 @@
-import { Close, Content, HeaderContainer, NavLinks, NavRegister, Overlay } from "./styles";
-import * as Dialog from '@radix-ui/react-dialog';
-
-
+import {
+  Close,
+  ContainerTextModal,
+  Content,
+  HeaderContainer,
+  HeaderMobile,
+  NavLinks,
+  NavRegister,
+  NavRegisterModal,
+  Overlay,
+  Trigger,
+} from "./styles";
+import * as Dialog from "@radix-ui/react-dialog";
 
 export const Header = () => {
-    return (
-        <HeaderContainer>
-            <Dialog.Root>
-                <Dialog.Trigger asChild>
-                    <img src="icon-menu.svg"/>
-                </Dialog.Trigger>
-                <Dialog.Portal>
-                    <Overlay />
-                    <Content>
-                    <Close><img src="icon-close-menu.svg"/></Close>
-                    <a>Features</a>
-                    <a>Company</a>
-                    <a>Careers</a>
-                    <a>About</a>
-                    <NavRegister>
-                        <a>Login</a>
-                        <button><a>Register</a></button>
-                    </NavRegister>
-                    </Content>
-                </Dialog.Portal>
-            </Dialog.Root>
-            <NavLinks>
-                <figure>
-                    <img src="logo.svg" alt="Logo" />
-                </figure>
-                <a>Features</a>
-                <a>Company</a>
-                <a>Careers</a>
-                <a>About</a>
-            </NavLinks>
-            <NavRegister>
-                <a>Login</a>
-                <button><a>Register</a></button>
-            </NavRegister>
-        </HeaderContainer>
-    )
-}
+  const isWidthDesktop = window.screen.width;
+
+  return (
+    <HeaderContainer>
+      {isWidthDesktop >= 1024 ? (
+        <>
+          <NavLinks>
+            <figure>
+              <img src="logo.svg" alt="Logo" />
+            </figure>
+            <a>Features</a>
+            <a>Company</a>
+            <a>Careers</a>
+            <a>About</a>
+          </NavLinks>
+          <NavRegister>
+            <a>Login</a>
+            <button>
+              <a>Register</a>
+            </button>
+          </NavRegister>
+        </>
+      ) : (
+        <HeaderMobile>
+          <figure>
+            <img src="logo.svg" alt="Logo" />
+          </figure>
+          <Dialog.Root>
+            <Dialog.Trigger asChild>
+              <img src="icon-menu.svg" />
+            </Dialog.Trigger>
+            <Dialog.Portal>
+              <Overlay />
+              <Content>
+                <Close>
+                  <img src="icon-close-menu.svg" />
+                </Close>
+                <ContainerTextModal>
+                  <a>
+                    Features <img src="icon-arrow-down.svg" alt="" />
+                  </a>
+                  <a>
+                    Company <img src="icon-arrow-down.svg" alt="" />
+                  </a>
+                  <a>Careers</a>
+                  <a>About</a>
+                </ContainerTextModal>
+                <NavRegisterModal>
+                  <a>Login</a>
+                  <button>
+                    <a>Register</a>
+                  </button>
+                </NavRegisterModal>
+              </Content>
+            </Dialog.Portal>
+          </Dialog.Root>
+        </HeaderMobile>
+      )}
+    </HeaderContainer>
+  );
+};
